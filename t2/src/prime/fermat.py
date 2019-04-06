c5fbb48ed1440c3819756f8d89fc75087ebe6b9f
Gustavo Olegário <gustavo-olegario@hotmail.com>
2019-04-06 19:28:01 -0300

    Documentation for fermat theorem finished

--- t2/src/prime/fermat.py
+++ t2/src/prime/fermat.py
@@ -1,4 +1,22 @@
 def fermat(n):
+    """ Implements the Fermat theorem. The
+    theorem says that if a number n is prime,
+    that any number between 1 and n - 1 pow
+    n - 1 divided by n will remaind exactly
+    one. If there is a, that do not attend
+    this condition, so, for sure, n it is
+    not a prime number. Otherwise, n is
+    , probably with a great chance,
+    prime.
+
+        Params:
+            n (int): the number that will
+            be checked if is prime or not.
+
+        Returns: boolean. If True, n is
+        probably a prime number. If False,
+        for certain is not a prime number.
+    """
     if n != 2 and n % 2 == 0:
         return False
 
@@ -10,4 +28,6 @@ def fermat(n):
     return True
 
 if __name__ == '__main__':
+    """ Uses 49 as base test.
+    """
     fermat(49)
