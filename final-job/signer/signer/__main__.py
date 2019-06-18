2dc4e1be22cbcbc12dd6b86fb0fcf48a8501edff
Gustavo Olegário <gustavo.olegario@dev.getmore.com.br>
2019-06-18 20:43:34 -0300

    Not using main currently

--- final-job/signer/signer/__main__.py
+++ final-job/signer/signer/__main__.py
@@ -6,12 +6,4 @@ from OpenSSL import crypto
 from os import environ
 
 if __name__ == '__main__':
-	load_dotenv()
-	CERT_PASSWORD = environ.get('CERT_PASSWORD')
-	certificate = open('./cert.p12', 'rb').read()
-	p12 = crypto.load_pkcs12(certificate, 'B3astInBlack')
-	pkey = p12.get_privatekey()
-	cert = p12.get_certificate()
-	data = open('./presentation.pdf', 'rb').read()
-	signature = crypto.sign(pkey, data, 'sha256')
-	print(crypto.verify(cert, signature, data, 'sha256'))
+	pass
